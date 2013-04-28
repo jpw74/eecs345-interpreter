722788e8606a491670a24fbfdd454bcebe32cf93
Jason Walsh <jason@jasonwalsh.us>
2013-04-28 14:42:21 -0500

    Reorganize some shit

---
+++ environment.scm
@@ -0,0 +1,117 @@
+; Creates a new environment
+; Takes no input
+(define new-environ
+  (lambda ()
+    '(()())))
+
+; Adds a layer onto the specified environment
+; Takes an environment to add a layer on
+(define layer
+  (lambda (environ)
+    (cons (new-environ) environ)))
+
+; Pops the first layer off of the specified environment
+; Takes an environment
+(define unlayer
+  (lambda (environ class instance)
+    (cdr environ)))
+
+; Adds a box to the environment
+; Takes a name, a box, and an envirnoment
+(define add-box
+  (lambda (name b environ)
+    (if (> (length environ) 2)
+      (cons (add-box name b (car environ)) (cdr environ))
+      (list (cons name (vars environ)) (cons b (vals environ))))))
+
+; Adds an element to the environment
+; Takes a variable name, a value for that variable, and an environment
+(define add
+  (lambda (variable value environ)
+    (let ((b (get-box variable environ)))
+      (if (eq? 'null b)
+        (if (> (length environ) 2)
+          (cons (add variable value (car environ)) (cdr environ))
+          (list (cons variable (vars environ)) (cons (box value) (vals environ))))
+        (begin
+          (set-box! b value) 
+          environ)))))
+
+; Adds an element to a 
+(define shallow-add
+  (lambda (variable value environ)
+    (if (> (length environ) 2)
+      (cons (add variable value (car environ)) (cdr environ))
+      (add variable value environ))))
+
+; Finds a specified element in the environment and returns its bound value
+; Takes a variable name and an environment
+(define lookup
+  (lambda (variable environ)
+    (if (> (length environ) 2)
+      (let ((val (lookup variable (car environ))))
+        (if (eq? val 'null)
+          (lookup variable (cdr environ))
+          val))
+      (cond
+        ((and (null? (vars environ)) (null? (vals environ))) 'null)
+        ((eq? variable (car (vars environ))) (unbox (car (vals environ))))
+        (else (lookup variable (list (cdr (vars environ)) (cdr (vals environ)))))))))
+
+(define cls-lookup
+  (lambda (name class environ)
+    (let* ((class-env (lookup class environ))
+           (static-var (lookup name (class.static-env class-env)))
+           (method (lookup name (class.method-env class-env)))
+           (parent (class.parent class-env)))
+      (if (eq? static-var 'null)
+        (if (eq? method 'null)
+          (cls-lookup name parent environ)
+          method)
+        static-var))))
+
+; Checks for redeclaration of variables
+; Takes a variable and an environment
+(define shallow-lookup
+  (lambda (variable environ)
+    (if (> (length environ) 2)
+      (lookup variable (car environ))
+      (cond
+        ((and (null? (vars environ)) (null? (vals environ))) 'null)
+        ((eq? variable (car (vars environ))) (unbox (car (vals environ))))
+        (else (lookup variable (list (cdr (vars environ)) (cdr (vals environ)))))))))
+
+; Returns the car of the specified environment (the variable name)
+(define vars
+  (lambda (environ)
+    (car environ)))
+
+; Returns the car of the cdr of the specfied environment (the value bound to the variable)
+(define vals
+  (lambda (environ)
+    (car (cdr environ))))
+
+; Returns the box containing the specified variable
+; Takes a variable and an environment
+(define get-box
+  (lambda (variable environ)
+    (if (> (length environ) 2)
+      (let ((b (get-box variable (car environ))))
+        (if (eq? b 'null)
+          (get-box variable (cdr environ))
+          b))
+      (cond
+        ((and (null? (vars environ)) (null? (vals environ))) 'null)
+        ((eq? variable (car (vars environ))) (car (vals environ)))
+        (else (get-box variable (list (cdr (vars environ)) (cdr (vals environ)))))))))
+
+; Returns the box containing the specified variable
+; Takes a variable and an environment
+(define shallow-get-box
+  (lambda (variable environ)
+    (if (> (length environ) 2)
+      (get-box variable (car environ))
+      (cond
+        ((and (null? (vars environ)) (null? (vals environ))) 'null)
+        ((eq? variable (car (vars environ))) (car (vals environ)))
+        (else (get-box variable (list (cdr (vars environ)) (cdr (vals environ)))))))))
---
+++ lib.scm
@@ -0,0 +1,97 @@
+; Helper function to assist evaluate-expr
+; Applies the appropriate mathematical operator
+(define atom-to-func
+  (lambda (atom)
+    (cond
+      ((eq? '* atom) *)
+      ((eq? '/ atom) quotient)
+      ((eq? '% atom) modulo)
+      ((eq? '+ atom) +)
+      ((eq? '- atom) -)
+      ((eq? '< atom) <)
+      ((eq? '<= atom) <=)
+      ((eq? '> atom) >) 
+      ((eq? '>= atom) >=) 
+      ((eq? '== atom) equal?))))
+
+(define class-def
+  (lambda (static-env instance-env method-env parent)
+    (list static-env instance-env method-env parent)))
+
+; Returns the operator of a statement
+; Takes a statement
+(define operator
+  (lambda (stmt)
+    (car stmt)))
+
+; Returns the first operand of a statement
+; Takes a statement
+(define operand1
+  (lambda (stmt)
+        (if (null? (cdr stmt))
+            '()
+        (car (cdr stmt)))))
+
+; Returns the second operand of a statement
+; Takes a statement
+(define operand2
+  (lambda (stmt)
+        (if (null? (cdr (cdr stmt)))
+            '()
+            (car (cdr (cdr stmt))))))
+
+; Returns the third operand of a statement
+; Takes a statement
+(define operand3
+  (lambda (stmt)
+    (if (null? (cdr (cdr (cdr stmt))))
+        '()
+        (car (cdr (cdr (cdr stmt)))))))
+
+; Returns the formal arguments of the function closure
+(define closure.formal
+  (lambda (closure)
+    (car closure)))
+
+; Returns the body of the function closure
+(define closure.body
+  (lambda (closure)
+    (car (cdr closure))))
+
+; Returns the environment of the function closure
+(define closure.environ
+  (lambda (closure)
+    (car (cdr (cdr closure)))))
+
+(define class.static-env
+  (lambda (class)
+    (car class)))
+
+(define class.instance-env
+  (lambda (class)
+    (cadr class)))
+
+(define class.method-env
+  (lambda (class)
+    (caddr class)))
+
+(define class.parent
+  (lambda (class)
+    (cadddr class)))
+
+(define box
+  (lambda (v)
+    (list v)))
+
+(define unbox
+  (lambda (b)
+    (car b)))
+
+(define set-box!
+  (lambda (b v)
+    (set-car! b v)))
+
+; A dummy function to be placed as a generic parameter
+(define identity
+  (lambda (v)
+    v))
